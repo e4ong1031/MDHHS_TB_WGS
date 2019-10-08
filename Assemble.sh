1c225838a8ced0ddd14b5375e2224a48afb568a0
Edison Ong <e4ong1031@gmail.com>
2019-10-08 18:20:56 -0400

    Clean up shell script.

--- Assemble.sh
+++ Assemble.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo docker run --rm -v $1:$1 -v $2:$2 -e DISPLAY={$DISPLAY} -v /tmp/.X11-unix:/tmp/.X11-unix:ro -e USERID=$UID -e USER=$USER -v $HOME/.Xauthority:/home/developer/.Xauthority e4ong1031/mdhhs_assemble:latest python3.6 Assemble.py -i $1 -o $2
+sudo docker run --rm -v $1:$1 -v $2:$2 e4ong1031/mdhhs_assemble:latest python3.6 Assemble.py -i $1 -o $2
